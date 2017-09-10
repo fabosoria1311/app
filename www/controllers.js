26780815d856b0fbceda7e4614630e69326df684
fabosoria1311 <31832213+fabosoria1311@users.noreply.github.com>
2017-09-10 11:30:17 -0500

    ts

--- www/controllers.js
+++ www/controllers.js
@@ -5,12 +5,12 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Sigsipamba', id: 1 },
+    { title: 'Cuyuja', id: 2 },
+    { title: 'Chimborazo', id: 3 },
+    { title: 'Santa Clara', id: 4 },
+    { title: 'Rinconada', id: 5 },
+    { title: 'La Perrera', id: 6 }
   ];
 })
 
